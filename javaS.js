d1619b9ff491620cf869c00a4dada17995b97e87
Squirrel18 <nando966@hotmail.es>
2016-12-30 21:11:02 -0500

    Init context webgl

--- javaS.js
+++ javaS.js
@@ -1,23 +1,38 @@
-var gl; // A global variable for the WebGL context
+var gl; //Variable global del contexto webGL
 
 function loadGL() {
+    //Seleccionar el objeto canvas
     var canvas = document.getElementById("canvasGL");
 
-    // Initialize the GL context
+    //Iniciar el contexto webGL
     gl = initWebGL(canvas);
     
-    // Only continue if WebGL is available and working
+    //Verifica si webGL puede iniciar
     if (!gl) {
         alert("No webGL");
         return;
     }
 
-    // Set clear color to black, fully opaque
+    //Conficura el color del canvas
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    // Enable depth testing
+    //Permite habilitar el test de profundidad
     gl.enable(gl.DEPTH_TEST);
-    // Near things obscure far things
+    //Las cosas cercanas obscurecen las cosas lejanas
     gl.depthFunc(gl.LEQUAL);
     // Clear the color as well as the depth buffer.
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+}
+
+function initWebGL(canvas) {
+  gl = null;
+  
+  //Escoge el contexto apropiado
+  gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+  
+  //Si no se seleciona ningún contexto
+  if (!gl) {
+    alert("Unable to initialize WebGL. Your browser may not support it.");
+  }
+  
+  return gl;
 }
