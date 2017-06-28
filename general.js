1cac7b26f8b2bd54d4d2a5cd452f80e401696b81
Squirrel18 <nando966@hotmail.es>
2017-06-28 17:12:00 -0500

    Setup up a webgl rendering

--- general.js
+++ general.js
@@ -1,38 +1,87 @@
-var gl; //Variable global del contexto webGL
+function start() {
+  var canvas = document.getElementById("glcanvas");
+  var gl = canvas.getContext("webgl");
 
-function loadGL() {
-    //Seleccionar el objeto canvas
-    var canvas = document.getElementById("canvasGL");
+  canvas.width = 640;
+  canvas.height = 480;
 
-    //Iniciar el contexto webGL
-    gl = initWebGL(canvas);
-    
-    //Verifica si webGL puede iniciar
-    if (!gl) {
-        alert("No webGL");
-        return;
-    }
 
-    //Conficura el color del canvas
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    //Permite habilitar el test de profundidad
-    gl.enable(gl.DEPTH_TEST);
-    //Las cosas cercanas obscurecen las cosas lejanas
-    gl.depthFunc(gl.LEQUAL);
-    // Clear the color as well as the depth buffer.
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  if(!gl) {
+    alert("not support for webgl contexts");
+    return;
+  } else {
+    alert("enjoy webgl");
+  }
+
+  //webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  var vertexShaderSource = document.getElementById("2d-vertex-shader").text;
+  var fragmentShaderSource = document.getElementById("2d-fragment-shader").text;
+ 
+  var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+  var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+  var program = createProgram(gl, vertexShader, fragmentShader);
+  var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+
+  var positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  var positions = [
+    0, 0,
+    0, 0.5,
+    0.7, 0,
+  ];
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+  gl.useProgram(program);
+  gl.enableVertexAttribArray(positionAttributeLocation);
+
+  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+  var size = 2;          // 2 components per iteration
+  var type = gl.FLOAT;   // the data is 32bit floats
+  var normalize = false; // don't normalize the data
+  var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+  var offset = 0;        // start at the beginning of the buffer
+  gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
+
+  var primitiveType = gl.TRIANGLES;
+  var offset = 0;
+  var count = 3;
+  gl.drawArrays(primitiveType, offset, count);
+
+
 }
 
-function initWebGL(canvas) {
-  gl = null;
-  
-  //Escoge el contexto apropiado
-  gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-  
-  //Si no se seleciona ningún contexto
-  if (!gl) {
-    alert("Unable to initialize WebGL. Your browser may not support it.");
+function createShader(gl, type, source) {
+  var shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (success) {
+    return shader;
+  } else {
+    alert("no create shaders");
   }
-  
-  return gl;
+ 
+  console.log(gl.getShaderInfoLog(shader));
+  gl.deleteShader(shader);
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+  var program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (success) {
+    return program;
+  } else {
+    alert("no create a program");
+  }
+ 
+  console.log(gl.getProgramInfoLog(program));
+  gl.deleteProgram(program);
 }
